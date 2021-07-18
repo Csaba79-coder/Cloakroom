8017b34f8a44b694ec90ee0714273e903fffbff0
Csaba79-coder <csabavadasz79@gmail.com>
2021-07-18 21:06:24 +0200

    finished with all tasks

---
+++ src/java/controller/ClothesController.java
@@ -0,0 +1,106 @@
+package controller;
+
+import clothes.*;
+
+import java.util.*;
+
+public class ClothesController {
+
+    public int countItems(Map<String, List<Clothes>> clothesListHashMap, String clothes) {
+        int i = 0;
+        if (i < clothesListHashMap.size()) {
+            do {
+                int counterKeep = 0;
+                for (int j = 0; j < clothesListHashMap.get(clothes).size(); j++) {
+                    counterKeep++;
+                }
+                return counterKeep;
+            } while (i < clothesListHashMap.size());
+        }
+        return -1;
+    }
+
+    public int countRemainingTShirts(List<TShirt> tShirtsList) {
+        int counter = 0;
+        List<String> tempList = new ArrayList<>();
+        for (int i = 0; i < tShirtsList.size(); i++) {
+            if (tShirtsList.get(i).getQualityMark().QUALITY_MARK.equals(1) || tShirtsList.get(i).getQualityMark().QUALITY_MARK.equals(2)) {
+                tempList.add(tShirtsList.get(i).getName());
+            }
+        }
+        for (int i = 0; i < tempList.size(); i++) {
+            counter++;
+        }
+        return counter;
+    }
+
+    public int countRemainingShirts(List<Shirt> shirtsList) {
+        int counter = 0;
+        List<String> tempList = new ArrayList<>();
+        for (int i = 0; i < shirtsList.size(); i++) {
+            if (shirtsList.get(i).getQualityMark().QUALITY_MARK.equals(1) || shirtsList.get(i).getQualityMark().QUALITY_MARK.equals(2)) {
+                tempList.add(shirtsList.get(i).getName());
+            }
+        }
+        for (int i = 0; i < tempList.size(); i++) {
+            counter++;
+        }
+        return counter;
+    }
+
+    public int countRemainingPants(List<Pants> pantsList) {
+        int counter = 0;
+        List<String> tempList = new ArrayList<>();
+        for (int i = 0; i < pantsList.size(); i++) {
+            if (pantsList.get(i).getQualityMark().QUALITY_MARK.equals(1) || pantsList.get(i).getQualityMark().QUALITY_MARK.equals(2)) {
+                tempList.add(pantsList.get(i).getName());
+            }
+        }
+        for (int i = 0; i < tempList.size(); i++) {
+            counter++;
+        }
+        return counter;
+    }
+
+    public int countRemainingJacket(List<Jacket> jacketList) {
+        int counter = 0;
+        List<String> tempList = new ArrayList<>();
+        for (int i = 0; i < jacketList.size(); i++) {
+            if (jacketList.get(i).getQualityMark().QUALITY_MARK.equals(1) || jacketList.get(i).getQualityMark().QUALITY_MARK.equals(2)) {
+                tempList.add(jacketList.get(i).getName());
+            }
+        }
+        for (int i = 0; i < tempList.size(); i++) {
+            counter++;
+        }
+        return counter;
+    }
+
+    public int countRemainingShoes(List<Shoes> shoesList) {
+        int counter = 0;
+        List<String> tempList = new ArrayList<>();
+        for (int i = 0; i < shoesList.size(); i++) {
+            if (shoesList.get(i).getQualityMark().QUALITY_MARK.equals(1) || shoesList.get(i).getQualityMark().QUALITY_MARK.equals(2)) {
+                tempList.add(shoesList.get(i).getName());
+            }
+        }
+        for (int i = 0; i < tempList.size(); i++) {
+            counter++;
+        }
+        return counter;
+    }
+
+    public int countRemainingSocks(List<Socks> socksList) {
+        int counter = 0;
+        List<String> tempList = new ArrayList<>();
+        for (int i = 0; i < socksList.size(); i++) {
+            if (socksList.get(i).getQualityMark().QUALITY_MARK.equals(1) || socksList.get(i).getQualityMark().QUALITY_MARK.equals(2)) {
+                tempList.add(socksList.get(i).getName());
+            }
+        }
+        for (int i = 0; i < tempList.size(); i++) {
+            counter++;
+        }
+        return counter;
+    }
+}
--- src/java/module/Processor.java
+++ src/java/module/Processor.java
@@ -1,8 +1,12 @@
 package module;
 
+import controller.ClothesController;
+import modell.QualityEnum;
 import utils.FileHandler;
 
+import java.io.File;
 import java.io.IOException;
+
 
 public class Processor {
 
@@ -12,11 +16,55 @@ public class Processor {
     public static void run() {
 
         try {
-            // FileHandler.read();
+            FileHandler.read();
             FileHandler.readToMap();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        ClothesController clothesController = new ClothesController();
 
+        int sumTShirts = clothesController.countItems(FileHandler.clothesListHashMap, "póló");
+        int sumShirts = clothesController.countItems(FileHandler.clothesListHashMap, "ing");
+        int sumPants = clothesController.countItems(FileHandler.clothesListHashMap, "nadrág");
+        int sumJacket = clothesController.countItems(FileHandler.clothesListHashMap, "kabát");
+        int sumShoes = clothesController.countItems(FileHandler.clothesListHashMap, "cipő");
+        int sumSocks = clothesController.countItems(FileHandler.clothesListHashMap, "zokni");
+        int sum = (sumTShirts + sumShirts + sumPants + sumJacket + sumShoes + sumSocks);
+
+        System.out.println(("I have all together " + sumTShirts + " T-shirts, "
+                + sumShirts + " shirts, "
+                + sumPants + " pants, "
+                + sumJacket + " jackets, "
+                + sumShoes + " shoes, "
+                + sumSocks + " socks."
+                + " That's altogether " + sum + " clothes!"));
+
+        int sumRemainingTShirts = clothesController.countRemainingTShirts(FileHandler.getTShirtsList());
+        int sumRemainingShirts = clothesController.countRemainingShirts(FileHandler.getShirtsList());
+        int sumRemainingPants = clothesController.countRemainingPants(FileHandler.getPantsList());
+        int sumRemainingJacket = clothesController.countRemainingJacket(FileHandler.getJacketList());
+        int sumRemainingShoes = clothesController.countRemainingShoes(FileHandler.getShoesList());
+        int sumRemainingSocks = clothesController.countRemainingSocks(FileHandler.getSocksList());
+
+        int sumAllRemainingStuffs = (sumRemainingShirts + sumRemainingTShirts + sumRemainingPants +
+                sumRemainingJacket + sumRemainingSocks + sumRemainingShoes);
+
+        System.out.println("Remaining T-shirts (quality mark as: 1 or 2): " + sumRemainingTShirts);
+        System.out.println("Remaining shirts (quality mark as: 1 or 2): " + sumRemainingShirts);
+        System.out.println("Remaining pants (quality mark as: 1 or 2): " + sumRemainingPants);
+        System.out.println("Remaining jackets (quality mark as: 1 or 2): " + sumRemainingJacket);
+        System.out.println("Remaining shoes (quality mark as: 1 or 2): " + sumRemainingShoes);
+        System.out.println("Remaining socks (quality mark as: 1 or 2): " + sumRemainingSocks);
+
+        System.out.println("The remained stuffs (mark: 1, 2): " + sumAllRemainingStuffs);
+
+        System.out.println("I donated and throw away from T-shits: " + (sumTShirts - sumRemainingTShirts));
+        System.out.println("I donated and throw away from shits: " + (sumShirts - sumRemainingShirts));
+        System.out.println("I donated and throw away from pants: " + (sumPants - sumRemainingPants));
+        System.out.println("I donated and throw away from jacket: " + (sumJacket - sumRemainingJacket));
+        System.out.println("I donated and throw away from shoes: " + (sumShoes - sumRemainingShoes));
+        System.out.println("I donated and throw away from socks: " + (sumSocks - sumRemainingSocks));
+
+        System.out.println("I donated and throw away from all stuffs: " + (sum - sumAllRemainingStuffs));
     }
 }
