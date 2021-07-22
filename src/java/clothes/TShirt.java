e1bcfaae435a36546c1fae4554d5f8464d631262
Csaba79-coder <csabavadasz79@gmail.com>
2021-07-22 20:43:52 +0200

    refactor make a public method to private

--- src/java/clothes/TShirt.java
+++ src/java/clothes/TShirt.java
@@ -12,7 +12,7 @@ public class TShirt extends Clothes {
         this(parts[0], parts[1], parts[2], parts[3], parts[4]);
     }
 
-    public boolean transformHasPatternFromString(String hasPattern) {
+    private boolean transformHasPatternFromString(String hasPattern) {
         if (hasPattern.equals("igen")) {
             return true;
         } else {
